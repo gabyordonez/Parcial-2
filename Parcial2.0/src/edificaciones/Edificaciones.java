24f564fb56a320d1138e327d7e5ed642e178d348
Gabriela Ordonez <00361516@uca.edu.sv>
2018-06-05 08:47:45 -0600

    Package edificaciones y centroDeMando

--- Parcial2.0/src/edificaciones/Edificaciones.java
+++ Parcial2.0/src/edificaciones/Edificaciones.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package centroDeMando;
+package edificaciones;
 
 /**
  *
  * @author gabyordonez
  */
-public class CentroDeMandoRusia {
+public interface Edificaciones {
     
 }
--- Parcial2.0/src/edificaciones/Recolectores/Recolectores.java
+++ Parcial2.0/src/edificaciones/Recolectores/Recolectores.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package centroDeMando;
+package edificaciones.Recolectores;
 
 /**
  *
  * @author gabyordonez
  */
-public class FactoryCentroDeMando {
+public interface Recolectores {
     
 }
